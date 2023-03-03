63f5622813e46a7a02b05e147e1cdb8ca17a2d45
Pa1mantri <114488271+Pa1mantri@users.noreply.github.com>
2023-03-04 02:00:59 +0530

    Add files via upload

---
+++ scripts/sta_1.tcl
@@ -0,0 +1,8 @@
+read_liberty sky130_fd_sc_hd__tt_025C_1v80.lib
+read_liberty sram_32_256_sky130A_TT_1p8V_25C.lib
+read_verilog vsdmemsoc.synth.v
+link_design vsdmemsoc
+set_units  -time ns
+read_sdc memsoc.sdc
+report_checks -path_delay min -group_count 1000 -endpoint_count 1000 -unique_paths_to_endpoint -fields {nets cap slew input_pins}
+
